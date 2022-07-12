a0e2c01c54666f812b1f677ec79fe1f02bd20319
Brent Abruzese <brent.abruzese@gmail.com>
2022-07-12 01:47:14 -0400

    some more initializing housekeeping

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import Head from 'next/head'
+import { ReactNode } from 'react'
 import Layout, { siteTitle } from '../components/layout/layout'
 
-export default function Home() {
+export default function Home({ children }: { children: ReactNode }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      <main>
+        {children}
+      </main>
     </Layout>
   )
 }
